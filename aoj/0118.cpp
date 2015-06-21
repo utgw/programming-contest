59a0d89cdc988443bbf0f8e6b16896d4c212eb70
UTAGAWA Kiki <utagawa.kiki@gmail.com>
2015-06-21 19:11:10 +0900

    accepted

--- aoj/0118.cpp
+++ aoj/0118.cpp
@@ -1,26 +1,52 @@
 //TODO
 #include <cstdio>
-#define REP(i,n) for(int i=0,i<n;i++)
-const int vec[3][2] = {{1,0},{1,1},{0,1}};
+#include <vector>
+#include <cstring>
+#define REP(i,n) for(int i=0;i<n;i++)
+using namespace std;
 
-int h,w;
-char mode=-1;
-char str[100][100];
-bool checked[100][100];
+char field[100][100];
+bool visited[100][100];
+int w,h;
 
-int solve(int x,int y){
-  if(mode==-1){
-    mode = str[y][x];
+void paint(int x,int y,char fruit){
+  const int vec[4][2] = {{1,0},{0,1},{0,-1},{-1,0}};
+  visited[y][x] = true;
+  REP(i,4){
+    if((y+vec[i][0]>=0)
+    &&(y+vec[i][0]<h)
+    &&(x+vec[i][1]>=0)
+    &&(x+vec[i][1]<w)
+    ){
+      if((field[y+vec[i][0]][x+vec[i][1]]==fruit)&&(!visited[y+vec[i][0]][x+vec[i][1]]))
+        paint(x+vec[i][1],y+vec[i][0],fruit);
+    }
   }
+  return;
 }
 
 int main(){
-  while(1){
+  while(true){
+    int cnt=0;
     scanf("%d%d\n",&h,&w);
-    if(h==0) break;
-    REP(i,h) scanf("%s\n",str[i]);
-    printf("%d\n",solve(0,0));
-    memset(checked,false,sizeof(checked));
+    if(w==0) break;
+
+    REP(i,h){
+      char str[100];
+      gets(str);
+      for(int j=0;str[j]!='\0';j++) field[i][j]=str[j];
+    }
+    // solve
+    REP(y,h){
+      REP(x,w){
+        if(visited[y][x]) continue;
+        cnt++;
+        paint(x,y,field[y][x]);
+      }
+    }
+    // end
+    printf("%d\n",cnt);
+    memset(visited,false,sizeof(visited));
   }
   return 0;
 }

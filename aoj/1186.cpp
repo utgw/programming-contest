47d3e3dc8d91a171eeccbd400f7efad5c60a8a16
UTAGAWA Kiki <utagawa.kiki@gmail.com>
2015-06-23 23:21:31 +0900

    modified

--- aoj/1186.cpp
+++ aoj/1186.cpp
@@ -1,45 +1,24 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-struct rectangle {
-  int h;
-  int w;
-  rectangle(int h, int w) :
-    h(h), w(w) {}
-};
-
+#include <cstdio>
+#define REP(i,n) for(int i=0;i<n;i++)
 int main(void) {
   const int inf = 889464;
-  int h, w;
-  int y, x;
-  int i, j;
-  vector<rectangle> dataset;
-  vector<rectangle>::iterator ds_iter;
-
-  while(true){
-    cin >> h >> w;
-    if (h == 0 && w == 0) break;
-    dataset.push_back(rectangle(h, w));
-  }
-
-  for(ds_iter = dataset.begin(); ds_iter != dataset.end(); ds_iter++){
-    rectangle rect = *ds_iter;
-    h = rect.h;
-    w = rect.w;
-    int dia = h*h + w*w;
-    int diam = inf;
-    for(i=1;i<=150;i++){  // よこ
-      for(j=1;j<i;j++){  // たて
-        int a = i*i+j*j;
-        if ((dia < a || (dia == a && j > h)) && a <= diam){
-          y = j;
-          x = i;
-          diam = a;
+  while(1){
+    int x,y,h,w;
+    scanf("%d%d\n",&h,&w);
+    if(h==0&&w==0)break;
+    int dia=h*h+w*w;
+    int diam=inf;
+    REP(i,150){
+      for(int j=1;j<i;j++){
+        int a=i*i+j*j;
+        if((dia<a||(dia==a&&j>h))&&a<=diam){
+          y=j;
+          x=i;
+          diam=a;
         }
       }
     }
-    cout << y << " " << x << endl;
+    printf("%d %d\n",y,x);
   }
   return 0;
 }

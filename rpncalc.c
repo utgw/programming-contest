63e74ef8c2aa28c445432bced41f6155263409c0
UTAGAWA Kiki <utagawa.kiki@gmail.com>
2015-07-16 15:34:04 +0900

    Reverse Polish Notation

---
+++ rpncalc.c
@@ -0,0 +1,49 @@
+﻿#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+  char buffer[256];
+  stack<int> st;
+  int a, b;
+
+  while(scanf("%s",buffer)!=EOF){
+    switch(buffer[0]){
+      case '+':
+        a = st.top();
+        st.pop();
+        b = st.top();
+        st.pop();
+        st.push(a+b);
+        break;
+      case '-':
+        a = st.top();
+        st.pop();
+        b = st.top();
+        st.pop();
+        st.push(a-b);
+        break;
+      case '*':
+        a = st.top();
+        st.pop();
+        b = st.top();
+        st.pop();
+        st.push(a*b);
+        break;
+      case '/':
+        a = st.top();
+        st.pop();
+        b = st.top();
+        st.pop();
+        st.push(a/b);
+        break;
+      default:
+        st.push(atoi(buffer));
+        break;
+    }
+  }
+  printf("%d\n", st.top());
+  return 0;
+}

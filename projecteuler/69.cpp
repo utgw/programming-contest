66d56eca6992c4cc0ed1dcd74640f0345c59a36b
UTAGAWA Kiki <utagawa.kiki@gmail.com>
2015-09-01 02:07:43 +0900

    Accepted

---
+++ projecteuler/69.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isprime(int n){
+  if(n <= 1) return false;
+  if(n == 2) return true;
+  for(int i=2;i*i<=n;i++)
+    if(n % i == 0) return false;
+  return true;
+}
+
+int main(){
+  int res = 1;
+  for(int i=2;res*i<=1000000;++i)
+    if(isprime(i)) res *= i;
+  for(int i=2;res*i<=1000000;++i)
+    res *= i;
+cout << res << endl;
+  return 0;
+}

c1585fda8ff81d65085cbc2d8e9ea067d618b4e8
UTAGAWA Kiki <utagawakiki@gmail.com>
2015-09-28 19:13:58 +0900

    WA

--- atcoder/code_festival_2015_qualA_c.cpp
+++ atcoder/code_festival_2015_qualA_c.cpp
@@ -1,37 +1,36 @@
-#include <stdio.h>
-#include <algorithm>
+#include <bits/stdc++.h>
 #define FOR(i,m,n) for(int i=m;i<(n);i++)
 #define REP(i,n) FOR(i,0,n)
 typedef long long ll;
 using namespace std;
-const int MAX_N = 10000;
-const ll INF = 2000000000;
-int N; ll T;
+const int MAX_N = 10001;
+
+int N, T;
 int homework[MAX_N][2];
 
-ll dfs(int n, ll t){
-  ll res = 0;
-  if(n == N){
-    if(t > T) return INF;
-    return 0;
-  }
-  res += min(dfs(n+1,t+homework[n][0]), dfs(n+1,t+homework[n][1])+1);
-  return res;
-}
-
-ll calculate(){
-  ll s = 0;
+int calculate(){
+  ll sum = 0;
+  int sub[MAX_N];
+  int change = 0;
   REP(i,N){
-    s += homework[i][1];
-    if(s > T) return -1;
+    sum += homework[i][1];
+    sub[i] = homework[i][0] - homework[i][1];
   }
-  return dfs(0,0);
+  if(sum > T) return -1;
+  sort(sub, sub + N);
+  REP(i,N){
+    if(sum + sub[i] <= T){
+      sum += sub[i];
+      change++;
+    }
+  }
+  return N - change;
 }
 
 int main(){
-  scanf("%d%lld",&N,&T);
+  scanf("%d%d",&N,&T);
   REP(i,N)
     scanf("%d%d",&homework[i][0],&homework[i][1]);
-  printf("%lld\n", calculate());
+  printf("%d\n", calculate());
   return 0;
 }

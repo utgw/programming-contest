72d1fccfb2fe888eefaa3fb6ccdb75bbff9ca387
UTAGAWA Kiki <utagawakiki@gmail.com>
2015-10-25 23:59:09 +0900

    AC

---
+++ atcoder/abc030_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  double A,B,C,D;
+  cin >> A >> B >> C >> D;
+  if(B/A > D/C)
+    cout << "TAKAHASHI" << endl;
+  else if(B/A < D/C)
+    cout << "AOKI" << endl;
+  else
+    cout << "DRAW" << endl;
+  return 0;
+}

a2cb4dca2a6c5fe18c7e3c4f99579dc7b6a7f206
UTAGAWA Kiki <utagawakiki@gmail.com>
2015-10-26 00:00:14 +0900

    AC

---
+++ aoj/2610.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  switch(n){
+    case 0:
+      cout << 1 << endl;
+      break;
+    case 1:
+      cout << 2 << endl;
+      break;
+    case 2:
+      cout << 1 << endl;
+      break;
+    default:
+      cout << 0 << endl;
+      break;
+  }
+  return 0;
+}

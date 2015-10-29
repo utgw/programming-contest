44f5a83befdf01a0c179c0f62702db6781afef85
UTAGAWA Kiki <utagawakiki@gmail.com>
2015-10-29 17:16:50 +0900

    AC

--- aoj/DPL_1_B.cpp
+++ aoj/DPL_1_B.cpp
@@ -1,28 +1,25 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
-using namespace std;
+#include <bits/stdc++.h>
 #define FOR(i,m,n) for(int i=m;i<(int)(n);i++)
 #define REP(i,n) FOR(i,0,n)
+using namespace std;
 const int MAX_NP = 100;
 const int MAX_W = 10000;
-int n,W;
-int w[MAX_NP];
-int v[MAX_NP];
-int dp[MAX_NP+1][MAX_W+1];
-
-int dfs(int i,int j){
-  int res;
-  if(dp[i][j]!=-1) return dp[i][j];
-  if(i==n) res = 0;
-  else if(j<w[i]) res = dfs(i+1,j);
-  else res = max(dfs(i+1,j),dfs(i+1,j-w[i])+v[i]);
-  return dp[i][j] = res;
-}
 
 int main(){
-  memset(dp,-1,sizeof(dp));
+  int n,W;
+  int w[MAX_NP];
+  int v[MAX_NP];
+  int dp[MAX_NP+1][MAX_W+1];
   cin >> n >> W;
   REP(i,n)cin >> v[i] >> w[i];
-  cout << dfs(0,W) << endl;
+  REP(i,n){
+    REP(j,W+1){
+      if(j < w[i])
+        dp[i+1][j] = dp[i][j];
+      else
+        dp[i+1][j] = max(dp[i][j], dp[i][j-w[i]]+v[i]);
+    }
+  }
+  cout << dp[n][W] << endl;
+  return 0;
 }

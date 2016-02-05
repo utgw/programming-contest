b933c93c56a4fb4145cc5096eb0525d2cc35a74e
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-02-05 02:46:35 +0000

    AC

--- aoj/0008.cpp
+++ aoj/0008.cpp
@@ -1,7 +1,15 @@
-#include <stdio.h>
-int main(){
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+
+int main(void){
   int n;
-  while(scanf("%d",&n) != EOF){
+  while(cin>>n){
     int result = 0;
     for(int a=0;a<=9;a++){
       for(int b=0;b<=9;b++){
@@ -10,7 +18,7 @@ int main(){
         }
       }
     }
-    printf("%d\n",result);
+    cout<<result<<endl;
   }
   return 0;
 }

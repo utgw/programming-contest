d88f98e80f6785e6375420b0b290e345f17cf0d4
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-02-09 10:52:44 +0000

    AC

---
+++ atcoder/abc/31/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+
+int main(void){
+  int A,D;cin>>A>>D;
+  cout<<max((A+1)*D,A*(D+1))<<endl;
+  return 0;
+}

9868ad79e7663954da5694f60b5bd4f642157110
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-03-13 18:12:30 +0900

    AC

--- aoj/1167.cpp
+++ aoj/1167.cpp
@@ -1,33 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define FOR(i,m,n) for(int i=m;i<(n);i++)
 #define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
 using namespace std;
-const int inf = 2147483647;
+typedef long long ll;
+const ll inf = INT_MAX;
 const double eps = 1e-8;
+const double pi = acos(-1.0);
 
-int main(){
-  const int nmax = 1000000;
-  int n;
-  vector<int> a, b;
-  // 10^6より小さい正三角形数・正四面体数を計算
-  FOR(i,1,nmax){
-    int c = i*(i+1)*(i+2)/6;
-    if(c < nmax){
-      a.push_back(c);
-      if(c % 2 == 1)
-        b.push_back(c);
-    }else break;
+int main(void){
+  // 1e6より小さい正四面体数を計算
+  const int NM=1000000;
+  int n=1;
+  vector<int> sa,sb;
+  while(1){
+    int k=n*(n+1)*(n+2)/6;
+    if(k>=NM)break;
+    sa.push_back(k);
+    if(k%2==1)sb.push_back(k);
+    n++;
   }
-  vector<int> dpa(nmax+1,inf);
-  vector<int> dpb(nmax+1,inf);
-  dpa[0] = dpb[0] = 0;
-  REP(i,nmax+1){
-    REP(j,a.size()){
-      if(i - a[j] >= 0) dpa[i] = min(dpa[i-a[j]]+1, dpa[i]);
-      if(j < b.size() && i - b[j] >= 0) dpb[i] = min(dpb[i-b[j]]+1, dpb[i]);
-    }
+  vector<ll> dpa(NM+1,inf),dpb(NM+1,inf);
+  dpa[0]=dpb[0]=0;
+  REP(i,NM+1)REP(j,sa.size()){
+    // cerr<<i<<" "<<j<<endl;
+    if(i-sa[j]>=0)dpa[i]=min(dpa[i],dpa[i-sa[j]]+1);
+    if(j<sb.size()&&i-sb[j]>=0)dpb[i]=min(dpb[i],dpb[i-sb[j]]+1);
   }
-  while(scanf("%d",&n),n != 0)
-    printf("%d %d\n", dpa[n], dpb[n]);
+  int N;
+  while(cin>>N,N){
+    cout<<dpa[N]<<" "<<dpb[N]<<endl;
+  }
   return 0;
 }

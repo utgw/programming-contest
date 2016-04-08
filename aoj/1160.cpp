8b8362ce50977e30b17342fd845102c815dba96c
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-08 16:42:50 +0900

    AC

--- aoj/1160.cpp
+++ aoj/1160.cpp
@@ -1,66 +1,41 @@
-#include <cstdio>
-#include <vector>
-#define REP(i,n) for(int i=0;i<n;i++)
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
 using namespace std;
+typedef long long ll;
 
-vector<vector<bool>> movable;
-vector<vector<bool>> visited;
-int w,h;
-int cnt=0;
-
-void paint(int x, int y){
-  const int vec[8][2] = {{1,0},{1,1},{0,1},{0,-1},{1,-1},{-1,-1},{-1,0},{-1,1}};
-  visited[y][x] = true;
-  REP(i,8){
-    if((y+vec[i][0]>=0)
-    &&(y+vec[i][0]<h)
-    &&(x+vec[i][1]>=0)
-    &&(x+vec[i][1]<w)
-    ){
-      if((movable[y+vec[i][0]][x+vec[i][1]])&&(!visited[y+vec[i][0]][x+vec[i][1]])) paint(x+vec[i][1],y+vec[i][0]);
+int main(void){
+  int w,h;
+  const int dh[][2] ={{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+  while(cin>>w>>h,w){
+    bool data[52][52];
+    bool visited[52][52];
+    memset(visited,false,sizeof visited);
+    REP(i,h)REP(j,w){
+      int s;cin>>s;
+      data[i][j]=s==1;
     }
-  }
-  return;
-}
-
-int main(){
-  while(true){
-    scanf("%d%d\n",&w,&h);
-    if(w==0) break;
-
-    REP(i,h){
-      char str[200];
-      gets(str);
-      vector<bool> set;
-      for(int i=0;str[i]!='\0';i++){
-        switch(str[i]){
-          case '1':
-            set.push_back(true);
-            break;
-          case '0':
-            set.push_back(false);
-            break;
+    int ans=0;
+    REP(i,h)REP(j,w){
+      if(visited[i][j])continue;
+      visited[i][j]=true;
+      if(!data[i][j])continue;
+      ans++;
+      stack<pair<int,int>> Q;
+      Q.push(make_pair(i,j));
+      while(!Q.empty()){
+        pair<int,int> now(Q.top());Q.pop();
+        int y=now.first,x=now.second;
+        visited[y][x]=true;
+        REP(k,8){
+          int nx=x+dh[k][0],ny=y+dh[k][1];
+          if(nx>=0&&nx<w&&ny>=0&&ny<h&&!visited[ny][nx]&&data[ny][nx])
+            Q.push(make_pair(ny,nx));
         }
       }
-      movable.push_back(set);
     }
-    REP(i,h) visited.push_back(vector<bool>(w,false));
-    // solve
-    REP(y,h){
-      REP(x,w){
-        if(visited[y][x]) continue;
-        if(movable[y][x]){
-          cnt++;
-          paint(x,y);
-        }else
-          visited[y][x] = true;
-      }
-    }
-    // end
-    printf("%d\n",cnt);
-    cnt = 0;
-    movable.erase(movable.begin(),movable.end());
-    visited.erase(visited.begin(),visited.end());
+    cout<<ans<<endl;
   }
   return 0;
 }

d7232ae10f740f297370beb8f4aead0d8e40ae32
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-09 22:17:25 +0900

    AC

---
+++ atcoder/abc/15/a.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+
+int main(void){
+  string a,b;cin>>a>>b;
+  if(a.size()>b.size())cout<<a<<endl;
+  else cout<<b<<endl;
+  return 0;
+}

c68739874713f51155145ae3e7f97b826bd46a53
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-15 21:55:21 +0900

    AC AOJ/0158

---
+++ aoj/0158.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+
+int main(void){
+  int n;
+  while(cin>>n,n){
+    int ans=0;
+    while(n>1){
+      if(n%2==0)n/=2;
+      else n=n*3+1;
+      ans++;
+    }
+    cout<<ans<<endl;
+  }
+  return 0;
+}

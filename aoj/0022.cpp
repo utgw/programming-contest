9997e725e92467d48906f00f0a643102e7e45c43
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-23 00:53:04 +0900

    AC aoj/0022

--- aoj/0022.cpp
+++ aoj/0022.cpp
@@ -1,24 +1,27 @@
-#include <stdio.h>
-#include <algorithm>
+#include<bits/stdc++.h>
 #define FOR(i,m,n) for(int i=m;i<(n);i++)
 #define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
 using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+const int di[][2] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
 
-int main(){
-  const int inf = 2147483647;
+int main(void){
   int n;
-  while(scanf("%d",&n),n != 0){
-    int S[5001];
-    int res = -inf;
-    S[0] = 0;
+  while(cin>>n,n){
+    vector<ll> s(n+1,0);
     FOR(i,1,n+1){
-      scanf("%d",&S[i]);
-      S[i] += S[i-1];
+      cin>>s[i];
+      s[i]+=s[i-1];
     }
-    REP(a,n)
-      FOR(b,a+1,n+1)
-        res = max(res, S[b] - S[a]);
-    printf("%d\n",res);
+    ll ans=-inf;
+    REP(l,n+1)FOR(r,l+1,n+1){
+      ans=max(ans,s[r]-s[l]);
+    }
+    cout<<ans<<endl;
   }
   return 0;
 }

5aca7ebf4dc9b1c0db4bcc6117b2078688804c67
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-05-07 12:43:36 +0900

    AC aoj/0112

---
+++ aoj/0112.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+const int di[][2] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+int main(void){
+  int n;
+  while(cin>>n,n){
+    vector<ll> a(n+1,0);
+    REP(i,n)cin>>a[i+1];
+    sort(ALL(a));
+    FOR(i,1,n)a[i+1]+=a[i];
+    FOR(i,1,n)a[i+1]+=a[i];
+    // REP(i,n)cout<<a[i]<<endl;
+    cout<<a[n-1]<<endl;
+  }
+  return 0;
+}

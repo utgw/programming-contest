4ebcc8c830d43d085b8ab977c42ef420155612cb
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-05-10 17:36:52 +0900

    AC aoj/2426

--- aoj/2426.cpp
+++ aoj/2426.cpp
@@ -11,22 +11,36 @@ const int di[][2] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
 
 int main(void){
   int n,m;cin>>n>>m;
-  vector<pair<ll,ll>> a(n);
+  vector<int> tx(n),ty(n);
+  REP(i,n)cin>>tx[i]>>ty[i];
+  vector<int> txd(tx),tyd(ty);
+  sort(ALL(tx));
+  sort(ALL(ty));
+  map<int,int> xtr,ytr;
   REP(i,n){
-    ll x,y;cin>>x>>y;
-    a[i]=make_pair(x,y);
+    xtr[tx[i]]=i+1;
+    ytr[ty[i]]=i+1;
   }
+  xtr[-1e9-1]=ytr[-1e9-1]=0;
+  xtr[1e9+1]=ytr[1e9+1]=n+1;
+  vector<vector<int>> trs(n+2,vector<int>(n+2,0));
+  REP(i,n)trs[xtr[txd[i]]][ytr[tyd[i]]]++;
+  vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+  FOR(i,1,n+2)FOR(j,1,n+2)dp[i][j]=dp[i][j-1]+dp[i-1][j]-dp[i-1][j-1]+trs[i][j];
   REP(i,m){
-    ll x1,y1,x2,y2;cin>>x1>>y1>>x2>>y2;
-    ll ans=0;
-    REP(j,n){
-      ll x=a[j].first,y=a[j].second;
-      if(x1<=x&&x<=x2&&y1<=y&&y<=y2){
-        // cerr<<x<<","<<y<<endl;
-        ans++;
-      }
-    }
-    cout<<ans<<endl;
+    int sx,sy,ex,ey;
+    cin>>sx>>sy>>ex>>ey;
+    int offset_x=xtr.lower_bound(sx)->second-1;
+    int offset_y=ytr.lower_bound(sy)->second-1;
+    int dest_x=xtr.upper_bound(ex)->second-1;
+    int dest_y=ytr.upper_bound(ey)->second-1;
+    // int offset_x=xtr.lower_bound(sx)->first;
+    // int offset_y=ytr.lower_bound(sy)->first;
+    // int dest_x=xtr.lower_bound(ex)->first;
+    // int dest_y=ytr.lower_bound(ey)->first;
+    // cout<<offset_x<<","<<offset_y<<endl;
+    // cout<<dest_x<<","<<dest_y<<endl;
+    cout<<dp[dest_x][dest_y]-dp[offset_x][dest_y]-dp[dest_x][offset_y]+dp[offset_x][offset_y]<<endl;
   }
   return 0;
 }

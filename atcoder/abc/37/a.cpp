12fff80284b10bc78c9868caa66deeb4dfc33cd8
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-05-15 00:11:06 +0900

    AC atcoder/abc/37/a,b,c

---
+++ atcoder/abc/37/a.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+const int di[][2] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+int main(void){
+  int a,b,c;cin>>a>>b>>c;
+  cout<<max(c/b,c/a)<<endl;
+  return 0;
+}
---
+++ atcoder/abc/37/c.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+const int di[][2] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+int main(void){
+  int n,k;cin>>n>>k;
+  vector<int> a(n);
+  vector<ll> s(n+1,0);
+  REP(i,n)cin>>a[i];
+  FOR(i,1,n+1)s[i]=a[i-1]+s[i-1];
+  // REP(i,n+1)cout<<s[i]<<endl;
+  ll sum=0;
+  REP(i,n-k+1)sum+=s[i+k]-s[i];
+  cout<<sum<<endl;
+  return 0;
+}

4ce06ab51032e49e8a1ebc5a8b9eaf8e1e7f1915
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-05-31 11:07:39 +0900

    AC atcoder/abc/21/D - 多重ループ

---
+++ atcoder/abc/21/d.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define FOR(i,m,n) for(int i=m;i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(x) (x).begin(),(x).end()
+using namespace std;
+typedef long long ll;
+const ll inf = INT_MAX;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+const int di[][2] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+const ll mod = 1e9+7;
+
+ll pow(ll a,ll b){
+  ll res=1;
+  for(;b>0;b>>=1){
+    if(b&1)res=(res*(a%mod))%mod;
+    a=((a%mod)*(a%mod))%mod;
+  }
+  return res;
+}
+
+ll inv(ll a){
+  return pow(a,mod-2);
+}
+
+int main(void){
+  int n;int k;cin>>n>>k;
+  vector<ll> fac(n+k+1,1);
+  REP(i,n+k){
+    fac[i+1]=(fac[i]*(i+1))%mod;
+  }
+  cout<<((fac[n+k-1]*inv(fac[k])%mod)%mod*inv(fac[n-1])%mod)%mod<<endl;
+  return 0;
+}

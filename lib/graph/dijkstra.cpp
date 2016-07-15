25f32098ea04883b61751c846f17350c98a05da9
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-07-15 19:51:01 +0900

    fix fatal bug

--- lib/graph/dijkstra.cpp
+++ lib/graph/dijkstra.cpp
@@ -1,45 +1,26 @@
-// Verified: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0155
-struct edge {
-  int to;
-  ll cost;
-};
-
+// Verified: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A
+typedef long long ll;
+typedef pair<ll, int> edge;
 typedef vector<edge> node;
 typedef vector<node> graph;
 
-pair<ll, vector<int>> dijkstra(graph& G, int start, int goal){
+const ll inf = LLONG_MAX / 3;
+
+vector<ll> dijkstra(graph& G, int start, int goal){
   const int n = G.size();
   vector<ll> dist(n, inf);
-  vector<int> prev(n, -1);
-  dist[start] = 0;
-  priority_queue<pair<ll, int>> next;
-  next.push(make_pair(0, start));
+  priority_queue<edge, vector<edge>, greater<edge>> next;
+  next.push(edge(0, start));
   while(!next.empty()){
     ll cost;
     int u;
-    tie(cost,u) = next.top();
+    tie(cost, u) = next.top();
     next.pop();
-    if(dist[u]<cost)
-      continue;
+    if(dist[u] < inf) continue;
+    dist[u] = cost;
     for(auto& e : G[u]){
-      int v = e.to;
-      ll alt = dist[u] + e.cost;
-      if(dist[v] > alt){
-        dist[v] = alt;
-        prev[v] = u;
-        next.push(make_pair(alt, v));
-      }
+      next.push(edge(cost+e.first, e.second));
     }
   }
-  vector<int> keiro;
-  if(prev[goal] != -1){
-    int mae = goal;
-    while(true){
-      keiro.push_back(mae + 1);
-      if(mae == start)break;
-      mae = prev[mae];
-    }
-    reverse(keiro.begin(), keiro.end());
-  }
-  return make_pair(dist[goal], keiro);
+  return dist;
 }

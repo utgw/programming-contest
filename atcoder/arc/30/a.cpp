a3dafc6b3682c0020a6d8221f3df605c7db20ddb
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-02-23 21:39:18 +0900

    AC arc 30 A

---
+++ atcoder/arc/30/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void) {
+  int n, k;
+  cin >> n >> k;
+  if (n >= 2 * k) 
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+  return 0;
+}
